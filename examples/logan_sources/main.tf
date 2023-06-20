ed3a937915ad9b27be6c33bc945542e1691d4f80
venkataramana manikyam <venkataramana.manikyam@oracle.com>
2023-06-20 12:13:07 +0530

    Update main.tf
    update module path

--- examples/logan_sources/main.tf
+++ examples/logan_sources/main.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 module "logan_sources" {
-  source = "./modules/logan_sources"
+  source = "../../modules/logan_sources"
   namespace = local.namespace
   schema_names = var.schema_names
   for_each = toset(split(",", var.products))

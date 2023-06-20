ad6ece3e7a41ab8a40a507a018595583abd5bd4f
venkataramana manikyam <venkataramana.manikyam@oracle.com>
2023-06-20 12:28:07 +0530

    Update main.tf
    remove namespace

--- examples/logan_sources/main.tf
+++ examples/logan_sources/main.tf
@@ -1,10 +1,5 @@
-locals {
-  namespace = ""
-}
-
 module "logan_sources" {
   source = "../../modules/logan_sources"
-  namespace = local.namespace
   schema_names = var.schema_names
   for_each = toset(split(",", var.products))
       path = format("%s/%s", "./contents/sources", each.value)

035a93d50bccab48283124e9c8f67bf32eb27d36
venkataramana manikyam <venkataramana.manikyam@oracle.com>
2023-06-20 12:00:33 +0530

    Create main.tf

---
+++ modules/logan_sources/main.tf
@@ -0,0 +1,14 @@
+# Import custom content after property values are substituted
+resource null_resource import_contents {
+  triggers = {
+    auth_type = "user"
+    profile_name = "test"
+    schema_names = format("%q", jsonencode(var.schema_names))
+    path = format("%q", var.path)
+    current_time = timestamp()
+  }
+
+  provisioner "local-exec" {
+    command = "python3 ./scripts/import_contents.py -a ${self.triggers.auth_type} -p ${self.triggers.profile_name} -e ${self.triggers.schema_names} -f ${self.triggers.path}"
+  }
+}

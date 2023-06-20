77062b0bc737c0b58b22a9a34ea9b3665f42a202
venkataramana manikyam <venkataramana.manikyam@oracle.com>
2023-06-20 12:05:28 +0530

    Create main.tf

---
+++ examples/logan_sources/main.tf
@@ -0,0 +1,11 @@
+locals {
+  namespace = "ax1zffkcg1fy"
+}
+
+module "logan_sources" {
+  source = "./modules/logan_sources"
+  namespace = local.namespace
+  schema_names = var.schema_names
+  for_each = toset(split(",", var.products))
+      path = format("%s/%s", "./contents/sources", each.value)
+}

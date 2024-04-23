c1386f3b94cb68b58c3d828e29daf51df354d595
Anderson Deluiz <anddeluiz@github>
2024-04-23 17:10:13 -0300

    Module alarm modifications
    * Removed default values for variables in optional()
    * Added support for overrides argument for resource type oci_monitoring_alarm
    * Added support for different prefix names for alarms and topics
    * Fixed display name for topics

--- modules/alarm/versions.tf
+++ modules/alarm/versions.tf
@@ -8,5 +8,8 @@ terraform {
       version = ">= 4.67.3"
     }
   }
-  required_version = ">= 1.3.0"
+  # required_version = ">= 1.3.0"
+  required_version = ">= 1.2.0"
+  # Only use below statment if terraform version <= 1.2.x
+  experiments = [ module_variable_optional_attrs ]
 }
